bb8e210ee82bf8de6ee8c27dddce7743270cc52c
Andrew Chin <achin@eminence32.net>
2015-04-11 15:16:11 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+#![feature(path_ext)] 
+
+use std::env::{home_dir, var_os, args_os};
+use std::path::{Path, PathBuf, AsPath};
+use std::fs::PathExt;
+use std::fs::create_dir;
+use std::ffi::{OsStr, AsOsStr};
+
+fn print_cd(p: &Path)  {
+    println!("cd '{}';", p.display());
+}
+
+fn setenv(var: &str, val: &Path) {
+    println!("export {}='{}';", var, val.display());
+}
+
+fn main() {
+
+
+    let mut mytmp: PathBuf = home_dir().expect("Unable to determine HOME directory");
+    mytmp.push("tmp");
+
+    let want_new: bool = args_os().any(|ref arg| arg == "-new");
+
+    // if a tmpdir is already set
+    let prevtmp = var_os("MYTMP");
+    if prevtmp.is_some() {
+        let _p = prevtmp.unwrap();
+        let prevtmp_path : &Path = Path::new(&_p);
+        if prevtmp_path.exists() && !want_new {
+            print_cd(prevtmp_path);
+            return;
+        }
+    }
+
+    for i in (0..99) {
+        let tmp = mytmp.join(format!("{0:02}", i));
+        if create_dir(&tmp).is_err() {
+            continue;
+        }
+        print_cd(&tmp);
+        setenv("MYTMP", &tmp);
+        return;
+
+    }
+
+
+}
